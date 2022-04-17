ac5f84533b21d5e6362ee9c0b976b454fde19623
platinum139 <platinum139.dev@gmail.com>
2022-04-17 22:39:27 +0300

    add pub-sub for returning query result

--- cmd/query-worker/queueHandler.go
+++ cmd/query-worker/queueHandler.go
@@ -21,6 +21,8 @@ func (handler *QueueHandler) Run() {
 		}
 
 		if request != "" {
+			message := "failed"
+
 			switch request {
 			case "products:bought":
 				handler.log.Printf("Incoming request: products:bought.")
@@ -32,7 +34,10 @@ func (handler *QueueHandler) Run() {
 				err = handler.redisClient.SetBoughtProductsCache(boughtProductsQuantity)
 				if err != nil {
 					handler.log.Printf("Failed to set bought products cache: %s\n", err)
+					break
 				}
+				handler.log.Println("BoughtProductsQuantity is got from db successfully.")
+				message = "success"
 
 			case "items:bought":
 				handler.log.Printf("Incoming request: items:bought.")
@@ -44,7 +49,16 @@ func (handler *QueueHandler) Run() {
 				err = handler.redisClient.SetBoughtItemsCache(boughtItemsQuantity)
 				if err != nil {
 					handler.log.Printf("Failed to set bought items cache: %s\n", err)
+					break
 				}
+				handler.log.Println("BoughtItemsQuantity is got from db successfully.")
+				message = "success"
+			}
+
+			if err := handler.redisClient.PublishResult(message); err != nil {
+				handler.log.Printf("Failed to publish message: %s\n", err)
+			} else {
+				handler.log.Printf("Message '%s' is published successfully.\n", message)
 			}
 		}
 	}
--- pkg/services/products.go
+++ pkg/services/products.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"log"
 	"warehouse-system/pkg/models"
 	"warehouse-system/pkg/redis"
@@ -17,19 +18,39 @@ func (ps *ProductService) GetBoughtProducts() ([]models.BoughtProductsQuantity, 
 		return nil, err
 	}
 
-	// if cache is empty, put request to a queue
-	if boughtProductsQuantity == nil {
-		ps.log.Println("'products:bought' cache is empty.")
-		if err := ps.redisClient.PutRequestToQueue("products:bought"); err != nil {
-			ps.log.Printf("Unable to put request to queue: %s\n", err)
-			return nil, err
-		}
-		ps.log.Println("Request is put to queue successfully.")
-		return nil, nil
+	if boughtProductsQuantity != nil {
+		ps.log.Println("'products:bought' cache is found.")
+		return boughtProductsQuantity, nil
 	}
 
-	ps.log.Println("'products:bought' cache is found.")
-	return boughtProductsQuantity, nil
+	// if cache is empty, put request to a queue
+	ps.log.Println("'products:bought' cache is empty.")
+
+	if err := ps.redisClient.PutRequestToQueue("products:bought"); err != nil {
+		ps.log.Printf("Unable to put request to queue: %s\n", err)
+		return nil, err
+	}
+	ps.log.Println("Request is put to queue successfully.")
+
+	message, err := ps.redisClient.SubscribeForResult()
+	if err != nil {
+		return nil, err
+	}
+	ps.log.Printf("Query worker has processed the request: %s\n", message)
+
+	if message == "failed" {
+		return nil, errors.New("failed to process query")
+	}
+
+	if message == "success" {
+		boughtProductsQuantity, err = ps.redisClient.GetBoughtProductsCache()
+		if err != nil {
+			return nil, err
+		}
+		return boughtProductsQuantity, nil
+	}
+
+	return nil, nil
 }
 
 func (ps *ProductService) GetBoughtItems() ([]models.BoughtItemsQuantity, error) {
@@ -38,19 +59,39 @@ func (ps *ProductService) GetBoughtItems() ([]models.BoughtItemsQuantity, error)
 		return nil, err
 	}
 
-	// if cache is empty, put request to a queue
-	if boughtItemsQuantity == nil {
-		ps.log.Println("'items:bought' cache is empty.")
-		if err := ps.redisClient.PutRequestToQueue("items:bought"); err != nil {
-			ps.log.Printf("Unable to put request to queue: %s\n", err)
-			return nil, err
-		}
-		ps.log.Println("Request is put to queue successfully.")
-		return nil, nil
+	if boughtItemsQuantity != nil {
+		ps.log.Println("'items:bought' cache is found.")
+		return boughtItemsQuantity, nil
 	}
 
-	ps.log.Println("'items:bought' cache is found.")
-	return boughtItemsQuantity, nil
+	// if cache is empty, put request to a queue
+	ps.log.Println("'items:bought' cache is empty.")
+
+	if err := ps.redisClient.PutRequestToQueue("items:bought"); err != nil {
+		ps.log.Printf("Unable to put request to queue: %s\n", err)
+		return nil, err
+	}
+	ps.log.Println("Request is put to queue successfully.")
+
+	message, err := ps.redisClient.SubscribeForResult()
+	if err != nil {
+		return nil, err
+	}
+	ps.log.Printf("Query worker has processed the request: %s\n", message)
+
+	if message == "failed" {
+		return nil, errors.New("failed to process query")
+	}
+
+	if message == "success" {
+		boughtItemsQuantity, err = ps.redisClient.GetBoughtItemsCache()
+		if err != nil {
+			return nil, err
+		}
+		return boughtItemsQuantity, nil
+	}
+	
+	return nil, nil
 }
 
 func NewProductService(log *log.Logger, redisClient *redis.Client) *ProductService {
