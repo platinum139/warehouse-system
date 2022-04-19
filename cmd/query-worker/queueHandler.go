faa326dfeccaf23b84933afbb682aec70ed3115f
platinum139 <platinum139.dev@gmail.com>
2022-04-19 12:22:24 +0300

    refactor handling queries

--- cmd/query-worker/queueHandler.go
+++ cmd/query-worker/queueHandler.go
@@ -25,61 +25,65 @@ func (handler *QueueHandler) Run() {
 			continue
 		}
 
-		if request != "" {
-			parts := strings.Split(request, ":")
-			token := parts[0]
-			handler.log.Printf("Received token: %s\n", token)
+		if request == "" {
+			continue
+		}
 
-			uid := parts[1]
-			handler.log.Printf("Received uid: %s\n", uid)
+		parts := strings.Split(request, ":")
+		token := parts[0]
+		uid := parts[1]
+		query := strings.Join(parts[2:], ":")
+		handler.log.Printf("Received: token %s, uid %s, query %s.\n", token, uid, query)
 
-			query := strings.Join(parts[2:], ":")
-			handler.log.Printf("Received query: %s\n", query)
+		var result string
+		switch query {
+		case "products:bought":
+			result = handler.handleGetBoughtProductsQuery()
+		case "items:bought":
+			result = handler.handleGetBoughtItemsQuery()
+		}
 
-			message := "failed"
-
-			switch query {
-			case "products:bought":
-				handler.log.Printf("Incoming request: products:bought.")
-				boughtProductsQuantity, err := handler.postgresClient.GetBoughtProductsQuantity()
-				if err != nil {
-					handler.log.Printf("Failed to get bought products quantity from postgres: %s\n", err)
-					break
-				}
-				expireAfter := time.Duration(handler.config.CacheExpireDuration) * time.Second
-				err = handler.redisClient.SetBoughtProductsCache(boughtProductsQuantity, expireAfter)
-				if err != nil {
-					handler.log.Printf("Failed to set bought products cache: %s\n", err)
-					break
-				}
-				handler.log.Println("BoughtProductsQuantity is got from db successfully.")
-				message = "success"
-
-			case "items:bought":
-				handler.log.Printf("Incoming request: items:bought.")
-				boughtItemsQuantity, err := handler.postgresClient.GetBoughtItemsQuantity()
-				if err != nil {
-					handler.log.Printf("Failed to get bought items quantity from postgres: %s\n", err)
-					break
-				}
-				expireAfter := time.Duration(handler.config.CacheExpireDuration) * time.Second
-				err = handler.redisClient.SetBoughtItemsCache(boughtItemsQuantity, expireAfter)
-				if err != nil {
-					handler.log.Printf("Failed to set bought items cache: %s\n", err)
-					break
-				}
-				handler.log.Println("BoughtItemsQuantity is got from db successfully.")
-				message = "success"
-			}
-
-			topic := fmt.Sprintf("%s:%s", token, uid)
-			if err := handler.redisClient.PublishResult(topic, message); err != nil {
-				handler.log.Printf("Failed to publish message: %s\n", err)
-			} else {
-				handler.log.Printf("Message '%s' is published successfully to topic %s.\n", message, topic)
-			}
+		topic := fmt.Sprintf("%s:%s", token, uid)
+		if err := handler.redisClient.PublishResult(topic, result); err != nil {
+			handler.log.Printf("Failed to publish result: %s\n", err)
+		} else {
+			handler.log.Printf("Result '%s' is published successfully to topic %s.\n", result, topic)
 		}
 	}
+}
+
+func (handler *QueueHandler) handleGetBoughtProductsQuery() string {
+	handler.log.Printf("Incoming request: products:bought.")
+	boughtProductsQuantity, err := handler.postgresClient.GetBoughtProductsQuantity()
+	if err != nil {
+		handler.log.Printf("Failed to get bought products quantity from postgres: %s\n", err)
+		return "failed"
+	}
+	expireAfter := time.Duration(handler.config.CacheExpireDuration) * time.Second
+	err = handler.redisClient.SetBoughtProductsCache(boughtProductsQuantity, expireAfter)
+	if err != nil {
+		handler.log.Printf("Failed to set bought products cache: %s\n", err)
+		return "failed"
+	}
+	handler.log.Println("BoughtProductsQuantity is got from db successfully.")
+	return "success"
+}
+
+func (handler *QueueHandler) handleGetBoughtItemsQuery() string {
+	handler.log.Printf("Incoming request: items:bought.")
+	boughtItemsQuantity, err := handler.postgresClient.GetBoughtItemsQuantity()
+	if err != nil {
+		handler.log.Printf("Failed to get bought items quantity from postgres: %s\n", err)
+		return "failed"
+	}
+	expireAfter := time.Duration(handler.config.CacheExpireDuration) * time.Second
+	err = handler.redisClient.SetBoughtItemsCache(boughtItemsQuantity, expireAfter)
+	if err != nil {
+		handler.log.Printf("Failed to set bought items cache: %s\n", err)
+		return "failed"
+	}
+	handler.log.Println("BoughtItemsQuantity is got from db successfully.")
+	return "success"
 }
 
 func NewQueueHandler(log *log.Logger, config *config.AppConfig, redis *redis.Client, postgres *postgres.Client) *QueueHandler {
